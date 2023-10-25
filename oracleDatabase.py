683253c35466390c583aae8f06796ec801dd56fc
Rodrigo Mendoza <A01720627@itesm.mx>
2023-10-25 15:55:30 -0600

    reformatting

--- oracleDatabase.py
+++ oracleDatabase.py
@@ -2,27 +2,31 @@ from tikTokAPI import TikTok
 import cx_Oracle
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 from io import BytesIO
 import requests
 from oracleCloud import OracleCloud
 
-class OracleDatabase:
 
+class OracleDatabase:
     def __init__(self):
         self.lib_dir = os.environ.get("LIB_DIR")
-        #cx_Oracle.init_oracle_client(lib_dir=self.lib_dir)
-        self.con = cx_Oracle.connect(user=os.environ.get("SCHEMA_USERNAME"), password=os.environ.get(
-            "SCHEMA_PASSWORD"), dsn=os.environ.get("SCHEMA_DSN"))
+        # cx_Oracle.init_oracle_client(lib_dir=self.lib_dir)
+        self.con = cx_Oracle.connect(
+            user=os.environ.get("SCHEMA_USERNAME"),
+            password=os.environ.get("SCHEMA_PASSWORD"),
+            dsn=os.environ.get("SCHEMA_DSN"),
+        )
         self.tiktok = TikTok()
         self.oracle_cloud = OracleCloud()
-    
+
+    # get the dictionary of transcripts from OracleCloud class and update the table in the DB
     def updateTranscript(self, transcript_dictionary):
         cursor = self.con.cursor()
         print(transcript_dictionary)
-        
+
         for key, value in transcript_dictionary.items():
-            
             query = f"""
             declare
             l_transcript CLOB;
@@ -39,44 +43,40 @@ class OracleDatabase:
                 print(e)
             self.con.commit()
             print("commited")
-            
+
         cursor.close()
 
-    def insertTikTok(self, keyword, count=30, offset=0, sort_type=0, publish_time=30):
-        self.tiktok.search(keyword, count=count, offset=offset,
-                           sort_type=sort_type, publish_time=publish_time)
-        dictionary = self.tiktok.video_dictionary
-
+    def insert_duality_view(self, value):
         cursor = self.con.cursor()
+        value = str(value).replace("'", "''")
+        # Insert the data in the duality view
+        query = f"""
+            
+            DECLARE full_text CLOB;
 
-        list_videos = [str(key)+'.mp4' for key, value in dictionary.items()]
-        
-        for key, value in dictionary.items():
+            BEGIN
+                full_text := '{value}';
 
-            value = str(value).replace("'", "''")
-            query = f"""
-                
-                DECLARE full_text CLOB;
+            BEGIN
+                INSERT INTO aweme_dv VALUES (full_text);
+            EXCEPTION WHEN DUP_VAL_ON_INDEX
+            THEN NULL;
+            END;
+            END;
 
-                BEGIN
-                    full_text := '{value}';
+        """
 
-                BEGIN
-                    INSERT INTO aweme_dv VALUES (full_text);
-                EXCEPTION WHEN DUP_VAL_ON_INDEX
-                THEN NULL;
-                END;
-                END;
+        try:
+            cursor.execute(query)
+        except:
+            pass
+        self.con.commit()
+        cursor.close()
 
-            """
-
-            try:
-                cursor.execute(query)
-            except:
-                pass
-            self.con.commit()
-
-            query = f"""
+    def insert_empty_blob(self, key):
+        cursor = self.con.cursor()
+        # Add empty blob to video_blob to be able to update it later
+        query = f"""
                 DECLARE
                 BEGIN
                 INSERT INTO VIDEO_BLOB VALUES ('{key}', empty_blob(), 'video/mp4', '{key}.mp4');
@@ -85,61 +85,94 @@ class OracleDatabase:
                 END;
             """
 
-            try:
-                cursor.execute(query)
-            except:
-                pass
-            self.con.commit()
-
-            select_query = f"""
-                SELECT (SELECT URL FROM VIDEO_URL WHERE AWEME_ID = '{key}' AND URL LIKE '%api16%' FETCH FIRST 1 ROWS ONLY) AS video_ur, DOWNLOADED_VIDEO FROM VIDEO_BLOB WHERE aweme_id = '{key}'
-                FOR UPDATE
-
-            """
-            cursor.execute(select_query)
-
-            cursor_fetch = cursor.fetchall()
-
-            for row in cursor_fetch:
-                (url, blob,) = row
-
-                request = requests.get(url=url)
-                
-                if request.status_code == 200:
-                    video_mp4 = request.content
-                    
-                    print('adding to OCI')
-                    self.oracle_cloud.insert_into_bucket(file_stream=video_mp4, name=key)
-                    print('added to OCI')
-                else:
-                    pass
-
-                video_mp4_stream = BytesIO(video_mp4)
-                print(len(video_mp4))
-                offset = 1
-                num_bytes_in_chunk = 65536
-                print('adding to blob')
-                while True:
-                    data = video_mp4_stream.read(num_bytes_in_chunk)
-                    if data:
-                        blob.write(data, offset)
-                    if len(data) < num_bytes_in_chunk:
-                        break
-                    offset += len(data)
-                print('added to blob')
-
-            self.con.commit()
-            
-        self.oracle_cloud.create_transcribe_job('pythonoracleapex', 'pythonoracleapex', '', list_videos)
-        
+        try:
+            cursor.execute(query)
+        except:
+            pass
+        self.con.commit()
         cursor.close()
-        
+
+    def insert_video_blob(self, key):
+        cursor = self.con.cursor()
+
+        select_query = f"""
+                        SELECT (SELECT URL FROM VIDEO_URL WHERE AWEME_ID = '{key}' AND URL LIKE '%api16%' FETCH FIRST 1 ROWS ONLY) AS video_ur, DOWNLOADED_VIDEO FROM VIDEO_BLOB WHERE aweme_id = '{key}'
+                        FOR UPDATE
+
+                    """
+        cursor.execute(select_query)
+
+        cursor_fetch = cursor.fetchall()
+
+        for row in cursor_fetch:
+            (
+                url,
+                blob,
+            ) = row
+
+            request = requests.get(url=url)
+
+            if request.status_code == 200:
+                video_mp4 = request.content
+
+                print("adding to OCI")
+                self.oracle_cloud.insert_into_bucket(file_stream=video_mp4, name=key)
+                print("added to OCI")
+            else:
+                pass
+
+            video_mp4_stream = BytesIO(video_mp4)
+            print(len(video_mp4))
+            offset = 1
+            num_bytes_in_chunk = 65536
+            print("adding to blob")
+            while True:
+                data = video_mp4_stream.read(num_bytes_in_chunk)
+                if data:
+                    blob.write(data, offset)
+                if len(data) < num_bytes_in_chunk:
+                    break
+                offset += len(data)
+            print("added to blob")
+
+        self.con.commit()
+        cursor.close()
+
+    # Create the transcription jobs from the videos extracted
+    def add_transcriptions(self, list_videos):
+        self.oracle_cloud.create_transcribe_job(
+            "pythonoracleapex", "pythonoracleapex", "", list_videos
+        )
+
         transcript_dictionary = self.oracle_cloud.process_transcribed_jobs()
+        # Update the video table with the transcription
         self.updateTranscript(transcript_dictionary=transcript_dictionary)
+
+    def insertTikTok(self, keyword, count=30, offset=0, sort_type=0, publish_time=30):
+        self.tiktok.search(
+            keyword,
+            count=count,
+            offset=offset,
+            sort_type=sort_type,
+            publish_time=publish_time,
+        )
+        dictionary = self.tiktok.video_dictionary
+
+        # list of videos to create the transcription job
+        list_videos = [str(key) + ".mp4" for key, value in dictionary.items()]
+
+        for key, value in dictionary.items():
+            self.insert_duality_view(value=value)
+
+            self.insert_empty_blob(key=key)
+
+            self.insert_video_blob(key=key)
+
+        self.add_transcriptions(list_videos=list_videos)
 
 
 if __name__ == "__main__":
     oracle = OracleDatabase()
-    oracle.insertTikTok(keyword='oracle apex', count=2)
+    oracle.insertTikTok(keyword="oracle apex", count=2)
     print(oracle.oracle_cloud.transcriptions_to_be_processed)
-    oracle.insertTikTok(keyword='oracle apex', count=2)
+    oracle.insertTikTok(keyword="oracle apex", count=2)
--- tikTokAPI.py
+++ tikTokAPI.py
@@ -1,4 +1,5 @@
 import requests
+
 # from dotenv import load_dotenv
 import os
 import urllib.request
@@ -14,7 +15,7 @@ class TikTok:
     def __init__(self):
         self.headers = {
             "X-RapidAPI-Key": os.environ.get("APIKEY"),
-            "X-RapidAPI-Host": os.environ.get("APIHOST")
+            "X-RapidAPI-Host": os.environ.get("APIHOST"),
         }
         self.video_dictionary = {}
         self.next_video_id = None
@@ -23,29 +24,6 @@ class TikTok:
         self.to_process = 0
         self.current_cursor = 0
 
-    def remove_emojis(self, data):
-        emoj = re.compile("["
-                          u"\U0001F600-\U0001F64F"  # emoticons
-                          u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                          u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                          u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                          u"\U00002500-\U00002BEF"  # chinese char
-                          u"\U00002702-\U000027B0"
-                          u"\U00002702-\U000027B0"
-                          u"\U000024C2-\U0001F251"
-                          u"\U0001f926-\U0001f937"
-                          u"\U00010000-\U0010ffff"
-                          u"\u2640-\u2642"
-                          u"\u2600-\u2B55"
-                          u"\u200d"
-                          u"\u23cf"
-                          u"\u23e9"
-                          u"\u231a"
-                          u"\ufe0f"  # dingbats
-                          u"\u3030"
-                          "]+", re.UNICODE)
-        return re.sub(emoj, '', data)
-
     def search(self, keyword, count=30, offset=0, sort_type=0, publish_time=30):
         self.video_dictionary = {}
         self.number_video = 0
@@ -53,24 +31,42 @@ class TikTok:
         self.next_video_id = None
         print("Searching TikTok videos...")
         url = "https://tokapi-mobile-version.p.rapidapi.com/v1/search/post"
-        querystring = {"keyword": keyword, "count": count, "sort_type": sort_type,
-                       "publish_time": publish_time, "offset": offset}
+        querystring = {
+            "keyword": keyword,
+            "count": count,
+            "sort_type": sort_type,
+            "publish_time": publish_time,
+            "offset": offset,
+        }
         response = requests.get(url, headers=self.headers, params=querystring)
         print("got response")
         response_json = response.json()
         response_videos = response_json["aweme_list"]
         for response_video in response_videos:
-            
             added_sound_music_info = {}
-            added_sound_music_info_tags = ['author', 'duration', 'title', 'audition_duration', 'id']
-            if response_video["added_sound_music_info"]  is not None: 
+            added_sound_music_info_tags = [
+                "author",
+                "duration",
+                "title",
+                "audition_duration",
+                "id",
+            ]
+            if response_video["added_sound_music_info"] is not None:
                 for tag in added_sound_music_info_tags:
-                    added_sound_music_info[tag] = response_video["added_sound_music_info"][tag]
+                    added_sound_music_info[tag] = response_video[
+                        "added_sound_music_info"
+                    ][tag]
 
             author = {}
-            author_tags = ['follower_count', 'nickname', 'search_user_name', 'following_count', 'uid']
+            author_tags = [
+                "follower_count",
+                "nickname",
+                "search_user_name",
+                "following_count",
+                "uid",
+            ]
 
-            if response_video["author"]  is not None: 
+            if response_video["author"] is not None:
                 for tag in author_tags:
                     author[tag] = response_video["author"][tag]
 
@@ -78,86 +74,73 @@ class TikTok:
             desc = response_video["desc"]
             desc_language = response_video["desc_language"]
 
-
             share_info = {}
-            share_info_tags = ['share_url']
+            share_info_tags = ["share_url"]
             for tag in share_info_tags:
-                share_info[tag] = response_video['share_info'][tag]
-
+                share_info[tag] = response_video["share_info"][tag]
 
             statistics = {}
-            statistics_tags = ['collect_count', 'comment_count', 
-                               'digg_count', 'download_count', 'forward_count', 'play_count', 
-                               'share_count', 'whatsapp_share_count']
-            
+            statistics_tags = [
+                "collect_count",
+                "comment_count",
+                "digg_count",
+                "download_count",
+                "forward_count",
+                "play_count",
+                "share_count",
+                "whatsapp_share_count",
+            ]
+
             for tag in statistics_tags:
-                statistics[tag] = response_video['statistics'][tag]
+                statistics[tag] = response_video["statistics"][tag]
 
             links_json = []
             video = {}
-            video_tags = ['download_addr', 'duration']
+            video_tags = ["download_addr", "duration"]
             for tag in video_tags:
-                if tag == 'download_addr':
-                    if response_video['video'][tag]['url_list'] is not None:
-                        for link in response_video['video'][tag]['url_list']:
-                            links_json.append({'url': link})
+                if tag == "download_addr":
+                    if response_video["video"][tag]["url_list"] is not None:
+                        for link in response_video["video"][tag]["url_list"]:
+                            links_json.append({"url": link})
                 else:
-                    video[tag] = response_video['video'][tag]
+                    video[tag] = response_video["video"][tag]
 
             text_extra = []
-            if response_video['text_extra']  is not None:
-                for hashtag in response_video['text_extra']:
+            if response_video["text_extra"] is not None:
+                for hashtag in response_video["text_extra"]:
                     if "hashtag_id" in hashtag and "hashtag_name" in hashtag:
-                        text_extra.append({'hashtag_id': hashtag['hashtag_id'], 'hashtag_name': hashtag['hashtag_name']})
-            
+                        text_extra.append(
+                            {
+                                "hashtag_id": hashtag["hashtag_id"],
+                                "hashtag_name": hashtag["hashtag_name"],
+                            }
+                        )
+
             # text_extra = response_video['text_extra']
-            aweme_id = response_video['aweme_id']
+            aweme_id = response_video["aweme_id"]
 
             print(aweme_id)
 
-            
-            self.video_dictionary[aweme_id] = {'added_sound_music_info': [added_sound_music_info], 'author': [author],
-                                               'create_time': create_time, 'desc': desc, 'desc_language': desc_language,
-                                               'share_info': [share_info], 'statistics': [statistics],
-                                               'video': [video], 'text_extra': text_extra, 'aweme_id': aweme_id, 'download_addr': links_json}
+            self.video_dictionary[aweme_id] = {
+                "added_sound_music_info": [added_sound_music_info],
+                "author": [author],
+                "create_time": create_time,
+                "desc": desc,
+                "desc_language": desc_language,
+                "share_info": [share_info],
+                "statistics": [statistics],
+                "video": [video],
+                "text_extra": text_extra,
+                "aweme_id": aweme_id,
+                "download_addr": links_json,
+            }
             print("added to dic")
-            
+
         """with open("response.json", "w") as outfile:
             json.dump(self.video_dictionary, outfile, indent=4)"""
         self.total_number = len(self.video_dictionary)
 
 
-    def increase_number_video(self):
-        self.number_video = self.total_number - len(self.video_dictionary)
-        try:
-            self.next_video_id = list(self.video_dictionary.keys())[0]
-        except Exception as e:
-            self.next_video_id = None
-            print(e)
-
-    def get_next_dictionary(self):
-        if self.next_video_id and len(self.video_dictionary) > 0:
-            return self.video_dictionary[self.next_video_id]
-
-    def delete_next_video(self):
-        if self.next_video_id is not None and len(self.video_dictionary) > 0:
-            self.video_dictionary.pop(self.next_video_id)
-
-    def get_total_number_of_videos(self):
-        return len(self.video_dictionary)
-
-    def get_video_dictionary(self):
-        return self.video_dictionary
-
-    def reset(self):
-        self.next_video_id = None
-        self.number_video = 0
-        self.total_number = 0
-        self.to_process = 0
-        self.current_cursor = 0
-        self.video_dictionary = {}
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     tiktok = TikTok()
-    tiktok.search('wine', count=2)
+    tiktok.search("wine", count=2)
